056b4e8df11e78a64194e337d29ebdd3acdf333b
Nayane Cristina <nayane.docarmo02@gmail.com>
2020-07-23 02:17:57 -0300

    Parte4/5 Concluida

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using E_Players_AspNetCore.Models;
+using System.IO;
 
 namespace E_Players_AspNetCore.Controllers
 {
@@ -28,11 +29,48 @@ namespace E_Players_AspNetCore.Controllers
             Equipe novaEquipe   = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse(form["IdEquipe"]);
             novaEquipe.Nome     = form["Nome"];
-            novaEquipe.Imagem   = form["Imagem"];
+           
+          /// <summary>
+          /// Puxar imagem dos arquivos do computador
+          /// </summary>
+            var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaEquipe.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem   = "padrao.png";
+            }
+            // Upload Final
 
             equipeModel.Create(novaEquipe);            
             ViewBag.Equipes = equipeModel.ReadAll();
 
+            return LocalRedirect("~/Equipe");
+        }
+/// <summary>
+/// excluir imagens 
+/// </summary>
+/// <param name="IdEquipe"></param>
+/// <returns>Apagar Arquivos/Equipe</returns>
+
+          [Route("[controller]/{ide}")]
+          public IActionResult Excluir(int ide)
+        {
+            equipeModel.Delete(ide);
+            ViewBag.Equipes = equipeModel.ReadAll();
             return LocalRedirect("~/Equipe");
         }
     }
--- Controllers/NoticiasController.cs
+++ Controllers/NoticiasController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using E_Players_AspNetCore.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace E_Players_AspNetCore.Controllers
 {
@@ -25,11 +26,48 @@ namespace E_Players_AspNetCore.Controllers
             novaNoticia.IdNoticias = Int32.Parse(form["IdNoticias"]);
             novaNoticia.Titulo  = form["Titulo"];
             novaNoticia.Texto = form["Texto"];
-            novaNoticia.Imagem   = form["Imagem"];
+           
+            /// <summary>
+          /// Puxar imagem dos arquivos do computador
+          /// </summary>
+            var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Noticias");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaNoticia.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaNoticia.Imagem   = "padrao.png";
+            }
+            // Upload Final
 
             noticiasModel.Create(novaNoticia);            
             ViewBag.Noticias = noticiasModel.ReadAll();
 
+            return LocalRedirect("~/Noticias");
+        }
+ /// <summary>
+/// excluir imagens 
+/// </summary>
+/// <param name="IdNoticias"></param>
+/// <returns>Apagar Arquivos/Notcias</returns>
+
+          [Route("[controller]/{idn}")]
+          public IActionResult Excluir(int idn)
+        {
+            noticiasModel.Delete(idn);
+            ViewBag.Noticias = noticiasModel.ReadAll();
             return LocalRedirect("~/Noticias");
         }
     }
